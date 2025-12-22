853c4ba8ce7ef9d3f88eeb93274cc19840ea7cc1
Dhirajkumar <kumardhiraj7720@gmail.com>
2025-12-22 15:52:38 +0530

    adding array2 in main

--- src/Dhiraj/Array2.java
+++ src/Dhiraj/Array2.java
@@ -1,4 +1,21 @@
 package Dhiraj;
 
 public class Array2 {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 45, 2, 89, 34}; // initialize array
+
+        int max = arr[0]; // assume first element as max
+
+        for (int i = 1; i < arr.length; i++) { // loop from second element
+            if (arr[i] > max) {               // compare current with max
+                max = arr[i];                 // update max
+            }
+        }
+
+        System.out.println("Maximum element: " + max); // output result
+    }
+
+
 }
