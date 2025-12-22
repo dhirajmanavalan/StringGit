5184c3733b019861f55cd6ef0985c7239f5ef7a8
Dhirajkumar <kumardhiraj7720@gmail.com>
2025-12-22 16:27:48 +0530

    array5 is stored in main

--- src/Dhiraj/Array5.java
+++ src/Dhiraj/Array5.java
@@ -1,4 +1,15 @@
 package Dhiraj;
 
 public class Array5 {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40}; // input array
+        int sum = 0;                  // sum variable
+
+        for (int i = 0; i < arr.length; i++) { // loop array
+            sum = sum + arr[i];                // add elements
+        }
+
+        System.out.println("Sum of Array: " + sum); // output
+    }
 }
