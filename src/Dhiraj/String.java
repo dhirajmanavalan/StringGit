229bc4d9e2d1e8b8ce9078ea8ae0806a1acb6f75
Dhirajkumar <kumardhiraj7720@gmail.com>
2025-12-22 15:19:32 +0530

    commit

---
+++ src/Dhiraj/String.java
@@ -0,0 +1,26 @@
+package Dhiraj;
+
+import java.lang.*;
+
+public class String {
+
+        public static void main(String[] args) {
+
+            java.lang.String str = "education";   // input string
+            int count = 0;              // vowel counter
+
+            for (int i = 0; i < str.length(); i++) {     // loop through string
+                char ch = str.charAt(i);                  // get each character
+
+                if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
+                    count++;                              // increase count
+                }
+            }
+
+            System.out.println("Vowel Count: " + count); // output
+        }
+    }
+
+
+
+
