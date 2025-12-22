859ef42455ddd20077d3a775747e1574a03c2145
Dhirajkumar <kumardhiraj7720@gmail.com>
2025-12-22 15:39:43 +0530

    adding to master

--- src/Dhiraj/String2.java
+++ src/Dhiraj/String2.java
@@ -1,4 +1,18 @@
 package Dhiraj;
 
 public class String2 {
+
+    public static void main(String[] args) {
+
+        java.lang.String str = "Java is very easy"; // input sentence
+        int count = 1;                   // word counter
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {  // space indicates new word
+                count++;
+            }
+        }
+
+        System.out.println("Word Count: " + count); // output
+    }
 }
