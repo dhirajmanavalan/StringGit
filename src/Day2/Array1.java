a1f01f276428caa8dfcd03aa920254e0b17a8ec4
Dhirajkumar <kumardhiraj7720@gmail.com>
2025-12-23 10:21:33 +0530

    added

--- src/Day2/Array1.java
+++ src/Day2/Array1.java
@@ -1,4 +1,23 @@
 package Day2;
 
+import java.util.Scanner;
+
 public class Array1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();        // size of array
+        int[] ar = new int[n];       // array creation
+
+        // taking input
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = sc.nextInt();
+        }
+
+        // printing array elements
+        for (int i = 0; i < ar.length; i++) {
+            System.out.print(ar[i] + " ");
+        }
+    }
 }
